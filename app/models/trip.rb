cc20fee8265044cdffd20dd1941f940b63215fc6
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-04-06 13:10:19 -0400

    Add fixture data for trips.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,8 +2,11 @@ class Trip < ActiveRecord::Base
   has_many :trip_points
   belongs_to :user
 
-  def finish
-    completion = true
-    walked_at = Time.now
-  end
+  # def finish
+  #   completion = true
+  # end
+  #
+  # def walked_now
+  #   walked_at = Time.now
+  # end
 end
--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -4,4 +4,13 @@ class TripTest < ActiveSupport::TestCase
   test "trips exist" do
     assert Trip
   end
+
+  test "trips can be finished" do
+    trip = trips(:chicago)
+    trip.finish
+    trip.walked_now
+    byebug
+    assert_equal trip.completion, true
+    assert_equal trip.walked_at, Time.now
+  end
 end
