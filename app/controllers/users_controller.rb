9bd78e06880cdb4845657d9d64f8e5a1bd9b880c
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-04-07 18:37:27 -0400

    Make dashboard action find by token.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy, :dashboard]
 
   def index
     @users = User.all
   end
 
   def dashboard
-    # @user = User.find_by(token: params[:token])
-    @user = User.find(1)
+    @user = User.find_by(token: params[:token])
+    # @user = User.find(1)
   end
 
   # GET /users/1
