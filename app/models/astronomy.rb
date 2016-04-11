94ad285d821e3f3ce22fb984e209bb43fa6047a3
Daniel Buompane <danielbuompane@gmail.com>
2016-04-11 12:01:52 -0400

    Create astronomy model for sunrise and sunset

---
+++ app/models/astronomy.rb
@@ -0,0 +1,14 @@
+class Astronomy
+  def initialize(latitude, longitude)
+    @response = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/astronomy/q/#{latitude, longitude}.json")
+  end
+
+  def sunrise
+  @astronomy["sun_phase"]["sunrise"]["hour"] + ":" + @astronomy["sun_phase"]["sunrise"]["minute"]
+end
+
+def sunset
+  @astronomy["sun_phase"]["sunset"]["hour"] + ":" + @astronomy["sun_phase"]["sunset"]["minute"]
+end
+
+end
