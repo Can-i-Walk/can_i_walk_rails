f8cdadb7556c2081df8af7c8b1ef2091871821aa
Daniel Buompane <danielbuompane@gmail.com>
2016-04-11 23:16:56 -0400

    Refactor models for weather api

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,6 +1,6 @@
 class Alert
   def initialize(latitude, longitude)
-    @response = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/alerts/q/#{latitude, longitude}.json")
+    @response = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/alerts/q/#{latitude},#{longitude}.json")
   end
 
   def alerts
