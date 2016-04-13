7a124296c92e8a5f784346acafc15e451695a094
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-04-13 10:40:57 -0400

    Write methods for rating class to return averages for different ratings.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,42 +2,21 @@ class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
 
-  # def self.ease_average(lat, long)
-  #   places = Place.within(0.25, :origin => [lat, long])
-  #   places = Place.within(5.0, :origin => [35.992892, -78.904498])
-  #   rated_places = []
-  #   ratings = []
-  #   places.each do |place|
-  #     rated_places << place if place.trip_points.where(place_type: "Ending Point").first
-  #   end
-  #   rated_places.each do |place|
-  #     ratings << place.ratings
-  #   end
-  #
-  #   end
-  #
-  #   Rating.joins('INNER JOIN places ON place.id = ratings.place_id').within().joins(:trip_points).where("trip_points.place_id = place.id")
-  #
-  #   Rating.where(rating.place.trip_points.where(place_type: "Ending Point")).within.pluck(:ease_rating).average
-  #   all_ratings = Rating.joins(:trip_points).where("place_type='Ending Point'")
-  #     eases = all_ratings.pluck(:ease_rating)
-  #
-  #   nearby_destination.each do |n|
-  #     @rated_places << n if n.trip_points.where(place_type: "Ending Point").first
-  #   end
-  # end
-  #
-  # def self.accessibility_average(lat, long)
-  #   places = Place.within(0.25, :origin => [lat, long])
-  # end
-  #
-  # def self.safety_average(lat, long)
-  #   places = Place.within(0.25, :origin => [lat, long])
-  # end
-  #
-  # def self.enjoyability_average(lat, long)
-  #   places = Place.within(0.25, :origin => [lat, long])
-  # end
+  def self.ease_average(lat, long)
+    Place.within(5.25, :origin => [lat, long]).joins(:ratings).average("ratings.ease_rating")
+  end
+
+  def self.accessibility_average(lat, long)
+    Place.within(5.25, :origin => [lat, long]).joins(:ratings).average("ratings.accessibility_rating")
+  end
+
+  def self.safety_average(lat, long)
+    Place.within(5.25, :origin => [lat, long]).joins(:ratings).average("ratings.safety_rating")
+  end
+
+  def self.enjoyability_average(lat, long)
+    Place.within(5.25, :origin => [lat, long]).joins(:ratings).average("ratings.enjoyability_rating")
+  end
 
 
 end
