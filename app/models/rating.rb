6f281a35bd784858c338f383a40c9f8765d2d851
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-04-13 11:58:16 -0400

    Add conditional to averaging methods in rating class to prevent rounding nil.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,18 +3,30 @@ class Rating < ActiveRecord::Base
   belongs_to :place
 
   def self.ease_average(lat, long)
-    Place.within(0.25, :origin => [lat, long]).joins(:ratings).average("ratings.ease_rating").round
+    ratings = Place.within(0.25, :origin => [lat, long]).joins(:ratings).average("ratings.ease_rating")
+    if ratings
+      ratings.round
+    end
   end
 
   def self.accessibility_average(lat, long)
-    Place.within(0.25, :origin => [lat, long]).joins(:ratings).average("ratings.accessibility_rating").round
+    ratings = Place.within(0.25, :origin => [lat, long]).joins(:ratings).average("ratings.accessibility_rating")
+    if ratings
+      ratings.round
+    end
   end
 
   def self.safety_average(lat, long)
-    Place.within(0.25, :origin => [lat, long]).joins(:ratings).average("ratings.safety_rating").round
+    ratings = Place.within(0.25, :origin => [lat, long]).joins(:ratings).average("ratings.safety_rating")
+    if ratings
+      ratings.round
+    end
   end
 
   def self.enjoyability_average(lat, long)
-    Place.within(0.25, :origin => [lat, long]).joins(:ratings).average("ratings.enjoyability_rating").round
+    ratings = Place.within(0.25, :origin => [lat, long]).joins(:ratings).average("ratings.enjoyability_rating")
+    if ratings
+      ratings.round
+    end
   end
 end
