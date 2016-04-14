af547b3efbd76108ccc3446bf4fdf647be3c404a
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-04-14 13:36:44 -0400

    Make it so averaging methods in rating model for accessibility type accept two parameters.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,28 +2,28 @@ class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
 
-  def self.average_cane()
+  def self.average_cane(lat, long)
     ratings = Place.within(0.25, :origin => [lat, long]).joins(user: :ratings).where('users.accessibility_type = "Cane"').average("ratings.accessibility_rating")
     if ratings
       ratings.round
     end
   end
 
-  def self.average_wheelchair()
+  def self.average_wheelchair(lat, long)
     ratings = Place.within(0.25, :origin => [lat, long]).joins(user: :ratings).where('users.accessibility_type = "Wheelchair"').average("ratings.accessibility_rating")
     if ratings
       ratings.round
     end
   end
 
-  def self.average_walker()
+  def self.average_walker(lat, long)
     ratings = Place.within(0.25, :origin => [lat, long]).joins(user: :ratings).where('users.accessibility_type = "Walker"').average("ratings.accessibility_rating")
     if ratings
       ratings.round
     end
   end
 
-  def self.average_scooter()
+  def self.average_scooter(lat, long)
     ratings = Place.within(0.25, :origin => [lat, long]).joins(user: :ratings).where('users.accessibility_type = "Scooter"').average("ratings.accessibility_rating")
     if ratings
       ratings.round
