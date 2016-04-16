d58cf31353470820c7272a56eac97f41e9995e53
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-04-16 15:52:18 -0400

    Remove the require and fetch from user params method on user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {}).permit(:email, :password, :password_confirmation, :name, :max_distance, :email_confirmed, :confirm_token, :accessibility_type)
+      params.permit(:email, :password, :password_confirmation, :name, :max_distance, :email_confirmed, :confirm_token, :accessibility_type)
     end
 end
