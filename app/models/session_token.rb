37ff347e6aaa18b5134f711034ee0be1ca575684
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-04-18 10:17:36 -0400

    Add schedular task for cleaning up old tokens.

--- app/models/session_token.rb
+++ app/models/session_token.rb
@@ -1,4 +1,8 @@
 class SessionToken < ActiveRecord::Base
   belongs_to :user
   validates :token, uniqueness: true
+
+  def self.cleanup_tokens
+    SessionToken.where("created_at < ?", 1.day.ago).destroy_all
+  end
 end
