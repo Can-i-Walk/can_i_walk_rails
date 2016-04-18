f64b79d3a844a079280f317734820b13b88b0afa
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-04-18 15:34:21 -0400

    JK wipe tokens every two days.

--- app/models/session_token.rb
+++ app/models/session_token.rb
@@ -3,6 +3,6 @@ class SessionToken < ActiveRecord::Base
   validates :token, uniqueness: true
 
   def self.cleanup_tokens
-    SessionToken.where("created_at < ?", 1.hour.ago).destroy_all
+    SessionToken.where("created_at < ?", 2.day.ago).destroy_all
   end
 end
